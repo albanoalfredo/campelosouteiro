9f5f135ac59cd5e050407cd90700b4769f46b57b
Alexandre Antunes <albanoalfredo@gmail.com>
2021-09-19 16:12:20 +0100

    added menu de navegacao

--- js/app.js
+++ js/app.js
@@ -1,27 +1,83 @@
-var btnscroll = document.getElementById("btnscroll");
-
-console.log("lido");
-
-// $(window).scroll(function() {
-//   if ($(window).scrollTop() > 300) {
-//     btnscroll.addClass('show');
-//     console.log("visto");
-  
-//   } else {
-//     btnscroll.removeClass('show');
-//     console.log("escondido");
-//   }
-// });
 
 
-window.onscroll = function() {viewOnScroll()};
-
-function viewOnScroll() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    btnscroll.classList.add('show');
-    console.log("mostra");
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function topNavToggle() {
+  var resp = document.getElementById("menu-topnav");
+  if (resp.className === "topnav hidden") {
+    resp.className += " responsive";
   } else {
-   btnscroll.classList.remove('show');
-    console.log("esconde");
+    resp.className = "topnav hidden";
   }
 }
+
+
+
+
+
+
+var btnscroll = document.getElementById("btnscroll");
+var scrollMenu = document.getElementById("menu-topnav")
+
+// evento para detectar scroll na janela
+
+window.onscroll = function() {
+  revealOnScroll();
+  menuOnScroll();
+};
+
+
+// funcao para aparecer o botao de whatsapp on scroll
+function revealOnScroll() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btnscroll.classList.add('show');
+
+
+  } else {
+   btnscroll.classList.remove('show');
+
+
+  }
+}
+
+// funcao para aparecer o menu ao fazer scroll para cima
+
+var prevScrollpos = window.pageYOffset;
+
+function menuOnScroll() {
+ var currentScrollPos = window.pageYOffset;
+   if (prevScrollpos > currentScrollPos) {
+     document.getElementById("menu-topnav").style.top = "0";
+   } else {
+     document.getElementById("menu-topnav").style.top = "-72px";
+   }
+   prevScrollpos = currentScrollPos;
+ }
+
+// show and hide menu on scroll - deprecated e merged com evento anterior
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("menu-topnav").style.top = "0";
+//   } else {
+//     document.getElementById("menu-topnav").style.top = "-72px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+// esconder menu quando se clica
+/*
+function devolverMenu() {
+  var y = document.getElementById("menu-topnav");
+
+  if (y.className === "menu") {
+    y.className += " a_descricao";
+  } else {
+    y.className = "menu";
+  }
+
+
+}
+*/
